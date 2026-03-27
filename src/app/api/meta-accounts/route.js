bc4ef058eccd0b54ba94f9518eef0f382a4b395b
Frank <francisco.hernandez4412@gmail.com>
2026-03-27 09:31:29 -0700

    fix build

--- src/app/api/meta-accounts/route.js
+++ src/app/api/meta-accounts/route.js
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server";
-import { getCredentials } from "../../../../lib/dbFunctions";
+import { getCredentials } from "../../../lib/dbFunctions";
 
 export const dynamic = "force-dynamic";
 export const revalidate = 0;
